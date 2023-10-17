93a55e781f6bf6934fd76a86c3a9b4e63bf96b31
Vero110 <veronicaalmaraz59@gmail.com>
2023-10-16 18:06:27 -0600

    Finalización de la practica4
    Agregar el tipo de rutas agrupadas para controlador.

--- PracticLaravel/routes/web.php
+++ PracticLaravel/routes/web.php
@@ -2,12 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\diarioController;
+/*
+//Rutas individuales para controlador
+Route::get('/', [diarioController::class, 'metodoInicio'])->name('apodoInicio');
+Route::get('/form', [diarioController::class, 'metodoform'])->name('apodoForm');
+Route::get('/memories', [diarioController::class, 'metodomemories'])->name('apodoMemories');
+*/
 
-route::get('/', [diarioController::class, 'metodoInicio'])->name('apodoInicio');
+//Rutas agrupadas para controlador 
+Route::controller(diarioController::class)->group(function(){
+    Route::get('/', 'metodoInicio')->name('apodoInicio');
+    Route::get('/form', 'metodoform')->name('apodoForm');
+    Route::get('/memories', 'metodomemories')->name('apodoMemories');
+});
 
-route::get('/form', [diarioController::class, 'metodoform'])->name('apodoForm');
-
-route::get('/memories', [diarioController::class, 'metodomemories'])->name('apodoMemories');
 
 
 
